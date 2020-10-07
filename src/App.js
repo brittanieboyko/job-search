87b45ded6504a166fe0ea72117ed77482e13c8f1
brittanieb <brittanie.boyko@gmail.com>
2020-10-07 15:55:12 -0400

    removed links to home and details

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import './App.css';
-import Home from './Components/Pages/Home/Home';
-import Details from './Components/Pages/Details/Details';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+import Home from "./Components/Pages/Home/Home";
+import Details from "./Components/Pages/Details/Details";
 
 function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/details">Details</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Switch>
           <Route path="/details">
             <Details />
