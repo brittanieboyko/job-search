e9c4ca226178992fae30f7c9143408854779582e
brittanieb <brittanie.boyko@gmail.com>
2020-10-07 15:46:38 -0400

    set up basic routing between home page and details page using react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+import Home from './Components/Pages/Home/Home';
+import Details from './Components/Pages/Details/Details';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/details">Details</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/details">
+            <Details />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
