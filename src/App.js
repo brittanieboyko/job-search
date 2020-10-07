9269daa99caf8ac415fa7d6792d1d31959dbf8e9
brittanieb <brittanie.boyko@gmail.com>
2020-10-07 16:18:20 -0400

    redirect unknown urls to no match page

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Components/Pages/Home/Home";
 import Details from "./Components/Pages/Details/Details";
+import NoMatch from "./Components/Pages/NoMatch/NoMatch";
 
 function App() {
   return (
     <Router>
-      <div>
         <Switch>
-          <Route path="/details">
+          <Route exact path="/details">
             <Details />
           </Route>
-          <Route path="/">
+          <Route exact path="/">
             <Home />
           </Route>
-
+          <Route path="*">
+            <NoMatch />
+          </Route>
         </Switch>
-      </div>
     </Router>
   );
 }
