f6adcb3310e7ba8cd4ff35102e9d29351425b97f
brittanieb <brittanie.boyko@gmail.com>
2020-10-09 20:40:55 -0400

    added bootstrap grid elements to home page card display

--- src/Components/JobsCard/JobsCard.js
+++ src/Components/JobsCard/JobsCard.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import './JobsCard.css';
 
 export default function JobsCard() {
   return (
     <Card className="text-center">
-      <Card.Header>Featured</Card.Header>
+      <Card.Header className="custom-card">Featured</Card.Header>
       <Card.Body>
         <Card.Title>Special title treatment</Card.Title>
         <Card.Text>
