b647eb545eecee07060d87b45340e71e8fc03fe1
brittanieb <brittanie.boyko@gmail.com>
2020-10-10 21:37:29 -0400

    import jobscard to jobscardcontainer and displayed a few

---
+++ src/Components/JobsCardContainer/JobsCardContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import JobsCard from "../JobsCard/JobsCard";
+
+export default function JobsCardContainer() {
+  return (
+    <>
+        <JobsCard />
+        <JobsCard />
+        <JobsCard />
+        <JobsCard />
+        <JobsCard />
+    </>
+  );
+}
