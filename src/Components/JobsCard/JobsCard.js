595c1e6cd0f3a1dbc174488e990149751f328400
brittanieb <brittanie.boyko@gmail.com>
2020-10-10 21:55:59 -0400

    used react router to link details button to navigate to the details page

--- src/Components/JobsCard/JobsCard.js
+++ src/Components/JobsCard/JobsCard.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import './JobsCard.css';
+import { Link } from "react-router-dom";
+import "./JobsCard.css";
 
 export default function JobsCard() {
   return (
     <Card className="text-center custom-card">
       <Card.Body>
-        <Card.Title>Job and Company Applied to</Card.Title>
-        <Button variant="light">See Details</Button>
+        <Card.Title>Job and Company Applied</Card.Title>
+        <Button variant="light">
+          <Link to="/details">See Details</Link>
+        </Button>
       </Card.Body>
       <Card.Footer className="text-muted">Updated 2 days ago</Card.Footer>
     </Card>
