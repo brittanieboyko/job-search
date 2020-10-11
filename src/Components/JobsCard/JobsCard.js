4330ddeab11a42ea4da259b3d950f8a935f31ad6
brittanieb <brittanie.boyko@gmail.com>
2020-10-10 21:35:12 -0400

    edited card details

--- src/Components/JobsCard/JobsCard.js
+++ src/Components/JobsCard/JobsCard.js
@@ -6,15 +6,11 @@ import './JobsCard.css';
 export default function JobsCard() {
   return (
     <Card className="text-center">
-      <Card.Header className="custom-card">Featured</Card.Header>
       <Card.Body>
-        <Card.Title>Special title treatment</Card.Title>
-        <Card.Text>
-          With supporting text below as a natural lead-in to additional content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Card.Title>Job and Company Applied to</Card.Title>
+        <Button variant="light">See Details</Button>
       </Card.Body>
-      <Card.Footer className="text-muted">2 days ago</Card.Footer>
+      <Card.Footer className="text-muted">Updated 2 days ago</Card.Footer>
     </Card>
   );
 }
