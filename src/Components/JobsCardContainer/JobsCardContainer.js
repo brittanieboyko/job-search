361476d7680d85689200566a24d4ecafee0a3d51
brittanieb <brittanie.boyko@gmail.com>
2020-10-11 21:18:05 -0400

    added some flexbox spacing to jobs container

--- src/Components/JobsCardContainer/JobsCardContainer.js
+++ src/Components/JobsCardContainer/JobsCardContainer.js
@@ -1,14 +1,15 @@
 import React from "react";
 import JobsCard from "../JobsCard/JobsCard";
+import './JobsCardContainer.css';
 
 export default function JobsCardContainer() {
   return (
-    <>
+    <div className="card-container">
         <JobsCard />
         <JobsCard />
         <JobsCard />
         <JobsCard />
         <JobsCard />
-    </>
+    </div>
   );
 }
