c779b867dc60c465273086c99a8a591f28841ac0
brittanieb <brittanie.boyko@gmail.com>
2020-10-13 18:35:58 -0400

    added router to index.js

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 // const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const apiPort = 3001
-const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-// const apiRouter = require('./routes/apiRoutes');
+const jobRouter = require("./routes/jobRouter");
+const apiPort = 3001;
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -21,22 +21,26 @@ if (process.env.NODE_ENV === "production") {
 // app.get("*", function (req, res) {
 //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 // });
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/jobs', {
-        useNewUrlParser: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true
-     })
-    .catch(e => {
-        console.error('Connection error', e.message)
-    })
+  .connect("mongodb://127.0.0.1:27017/jobs", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .catch((e) => {
+    console.error("Connection error", e.message);
+  });
 
-mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+mongoose.connection.on(
+  "error",
+  console.error.bind(console, "MongoDB connection error:")
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.use("/api", jobRouter);
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
 
 module.exports = app;
