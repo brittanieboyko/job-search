a1b8e44d8bf27e779253d51d36c5a6c76bdbce42
brittanieb <brittanie.boyko@gmail.com>
2020-10-19 11:50:50 -0400

    fixed model function names in jobRoutes

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -4,10 +4,10 @@ const JobCtrl = require("../controllers/JobController");
 
 const router = express.Router();
 
-router.post("/job", JobCtrl.createJob);
+router.post("/job", JobCtrl.addJob);
 router.put("/job/:id", JobCtrl.updateJob);
 router.delete("/job/:id", JobCtrl.deleteJob);
-router.get("/job/:id", JobCtrl.getJobById);
-router.get("/jobs", JobCtrl.getJobs);
+router.get("/job/:id", JobCtrl.findJobById);
+router.get("/jobs", JobCtrl.findAllJobs);
 
 module.exports = router;
