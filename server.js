bd767fe73bcbe235e077231e4e7287c08bc05fa7
brittanieb <brittanie.boyko@gmail.com>
2020-10-19 11:49:25 -0400

    import new db file into server.js

--- server.js
+++ server.js
@@ -1,44 +1,17 @@
-const express = require("express");
-const path = require('path');
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-const jobRouter = require("./routes/jobRouter");
-const mongoose = require("mongoose");
+const db = require('./db')
 
-const app = express();
+const app = express()
+const apiRouter = require('./routes/jobRouter');
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
 
-// app.use('/api', apiRouter);
-// app.get("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use(bodyParser.json())
 
-mongoose
-  .connect("mongodb://127.0.0.1:27017/jobs", {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .catch((e) => {
-    console.error("Connection error", e.message);
-  });
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-mongoose.connection.on(
-  "error",
-  console.error.bind(console, "MongoDB connection error:")
-);
-
-app.use("/api", jobRouter);
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
-
-module.exports = app;
+app.use('/api', apiRouter);
