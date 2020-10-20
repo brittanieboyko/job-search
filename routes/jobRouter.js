d404b1ca8d992ca6dea774e69a3fcde78578814a
brittanieb <brittanie.boyko@gmail.com>
2020-10-20 10:58:33 -0400

    fixed getJobs route

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/job", JobCtrl.createJob);
 router.put("/job/:id", JobCtrl.updateJob);
 router.delete("/job/:id", JobCtrl.deleteJob);
-router.get("/job/:id", JobCtrl.getJobs);
-router.get("/jobs", JobCtrl.getJobById);
+router.get("/jobs", JobCtrl.getJobs);
+router.get("/job/:id", JobCtrl.getJobById);
 
 module.exports = router;
