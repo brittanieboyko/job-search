438ac95946ca82aaacae0d36ae4ac7f98ffe74b4
brittanieb <brittanie.boyko@gmail.com>
2020-10-21 10:39:48 -0400

    reorganized folder structure into client and server sections

--- src/Components/DetailsContainer/DetailsContainer.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function DetailsContainer() {
-    return <h2>Details Container</h2>;
-  }
--- src/Components/ToDoContainer/ToDoContainer.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function ToDoContainer() {
-    return <h2>To Do</h2>;
-  }
