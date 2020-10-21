b2285ab24e0b189435e57ffd0bf8adca9a48a7f9
brittanieb <brittanie.boyko@gmail.com>
2020-10-21 10:53:13 -0400

    file structuring

--- client/src/Components/Pages/Details/Details.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import DetailsContainer from '../../DetailsContainer/DetailsContainer';
-
-export default function Details() {
-    return <DetailsContainer />;
-  }
--- client/src/Components/Pages/NoMatch/NoMatch.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function NoMatch() {
-    return <h2>The page does not exist</h2>;
-  }
