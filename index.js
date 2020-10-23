91bb39419e1414215517825b859d3d85f2e99f1a
brittanieb <brittanie.boyko@gmail.com>
2020-10-23 15:42:56 -0400

    renamed server.js to index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const apiPort = 3000;
+
+const db = require('./server/db/db')
+const apiRouter = require('./server/routes/jobRouter');
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use(bodyParser.json())
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.use('/api', apiRouter);
+
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+module.exports = app;
