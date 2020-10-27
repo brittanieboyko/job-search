38ff6ab6321f4ddc6624bd425439acfb3162acd7
brittanieb <brittanie.boyko@gmail.com>
2020-10-26 20:26:57 -0400

    pass in unifiedTopology:true to Mongo client constructor to silence warning

--- server/db/db.js
+++ server/db/db.js
@@ -1,11 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/jobs', { useNewUrlParser: true })
-    .catch(e => {
-        console.error('Connection error', e.message)
-    })
+  .connect(
+    "mongodb://127.0.0.1:27017/jobs",
+    { useNewUrlParser: true },
+    { useUnifiedTopology: true }
+  )
+  .catch((e) => {
+    console.error("Connection error", e.message);
+  });
 
-const db = mongoose.connection
+const db = mongoose.connection;
 
-module.exports = db
+module.exports = db;
