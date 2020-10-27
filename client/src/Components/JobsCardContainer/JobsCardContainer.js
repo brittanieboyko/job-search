ae4f4b781b361a3e1f34912b17df4efa5bb46b5a
brittanieb <brittanie.boyko@gmail.com>
2020-10-27 10:55:13 -0400

    used axios to call getAllJobs route in jobsContainer component. Currently just logging all jobs in db to the console

--- client/src/Components/JobsCardContainer/JobsCardContainer.js
+++ client/src/Components/JobsCardContainer/JobsCardContainer.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import JobsCard from "../JobsCard/JobsCard";
-import './JobsCardContainer.css';
+import api from "../../api";
+import "./JobsCardContainer.css";
 
-export default function JobsCardContainer() {
+const JobsCardContainer = () => {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    loadJobs();
+  }, []);
+
+  const loadJobs = () => {
+    api
+      .getAllJobs()
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="card-container">
-        <JobsCard />
-        <JobsCard />
-        <JobsCard />
-        <JobsCard />
-        <JobsCard />
+      <JobsCard />
+      <JobsCard />
+      <JobsCard />
+      <JobsCard />
+      <JobsCard />
     </div>
   );
-}
+};
+
+export default JobsCardContainer;
