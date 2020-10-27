51474692e115a101ec29d183b653cf461870afae
brittanieb <brittanie.boyko@gmail.com>
2020-10-27 10:28:26 -0400

    added button to noMatch.js with link to take user back to home page

--- client/src/Pages/NoMatch/NoMatch.js
+++ client/src/Pages/NoMatch/NoMatch.js
@@ -1,5 +1,14 @@
-import React from 'react';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 
 export default function NoMatch() {
-    return <h2>The page does not exist</h2>;
-  }
+  return (
+    <>
+      <h2>The page does not exist</h2>
+      <Button variant="light">
+        <Link to="/">Go to Home Page</Link>
+      </Button>
+    </>
+  );
+}
