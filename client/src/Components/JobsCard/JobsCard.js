c193aca3209a84ada9701fd0b0b90e90aeddeb09
brittanieb <brittanie.boyko@gmail.com>
2020-10-28 13:50:36 -0400

    fixed formatting

--- client/src/Components/JobsCard/JobsCard.js
+++ client/src/Components/JobsCard/JobsCard.js
@@ -5,18 +5,18 @@ import { Link } from "react-router-dom";
 import "./JobsCard.css";
 
 const JobsCard = (props) => {
-  console.log("props ->",props)
+  console.log("props ->", props);
   return (
     <Card className="text-center custom-card">
       <Card.Body>
-  <Card.Title>{props.job.title}</Card.Title>
+        <Card.Title>{props.job.title}</Card.Title>
         <Button variant="light">
           <Link to="/details">See Details</Link>
         </Button>
       </Card.Body>
-  <Card.Footer className="text-muted">{props.job.dateApplied}</Card.Footer>
+      <Card.Footer className="text-muted">{props.job.dateApplied}</Card.Footer>
     </Card>
   );
-}
+};
 
 export default JobsCard;
