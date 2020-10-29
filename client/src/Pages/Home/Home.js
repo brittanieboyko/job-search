0bf947cc50ae17e0c40f1ec15fddf3e11d229e01
brittanieb <brittanie.boyko@gmail.com>
2020-10-29 16:25:12 -0400

    import addButton on to Home Page

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -2,11 +2,13 @@ import React from 'react';
 import JobsCardContainer from '../../Components/JobsCardContainer/JobsCardContainer';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import AddButton from '../../Components/AddButton/AddButton';
 
 const Home = () => {
     return (
       <Container>
         <h2>Home</h2>
+        <AddButton />
         <Row>
           <JobsCardContainer />
         </Row>
