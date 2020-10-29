8dc448636d71b5e30ba90f0d6679ffc3b7a0d288
brittanieb <brittanie.boyko@gmail.com>
2020-10-29 17:05:09 -0400

    added a form to the add job modal

--- client/src/Components/AddModal/AddModal.js
+++ client/src/Components/AddModal/AddModal.js
@@ -1,42 +1,61 @@
 import React, { useState } from "react";
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const AddModal = () => {
-    const [show, setShow] = useState(false);
-  
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-  
-    return (
-      <>
-        <Button variant="primary" onClick={handleShow}>
-          Add A Job!
-        </Button>
-  
-        <Modal
-          show={show}
-          onHide={handleClose}
-          backdrop="static"
-          keyboard={false}
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>Modal title</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            I will not close if you click outside me. Don't even try to press
-            escape key.
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Cancel
-            </Button>
-            <Button variant="primary">Save Job</Button>
-          </Modal.Footer>
-        </Modal>
-      </>
-    );
-  }
-  
+  const [show, setShow] = useState(false);
 
-  export default AddModal;
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Add A Job!
+      </Button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group as={Row} controlId="formPlaintextEmail">
+              <Form.Label column sm="2">
+                Job Title:
+              </Form.Label>
+              <Col sm="10">
+                <Form.Control type="text" placeholder="Title" />
+              </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} controlId="formPlaintextPassword">
+              <Form.Label column sm="2">
+                Date Applied:
+              </Form.Label>
+              <Col sm="10">
+                <Form.Control type="password" placeholder="Date" />
+              </Col>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button variant="primary">Save Job</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default AddModal;
