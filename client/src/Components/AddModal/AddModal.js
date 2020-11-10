776c63984b418fbf67c0ac3485cec6daee77f001
brittanieb <brittanie.boyko@gmail.com>
2020-11-10 13:10:09 -0500

    imported Form component into Modal component

--- client/src/Components/AddModal/AddModal.js
+++ client/src/Components/AddModal/AddModal.js
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-import Form from "react-bootstrap/Form";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import Form from "../Form/Form";
 
 const AddModal = () => {
   const [show, setShow] = useState(false);
@@ -27,25 +25,7 @@ const AddModal = () => {
           <Modal.Title>Add a Job</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
-            <Form.Group as={Row} controlId="formPlaintextEmail">
-              <Form.Label column sm="2">
-                Job Title:
-              </Form.Label>
-              <Col sm="10">
-                <Form.Control type="text" placeholder="Title" />
-              </Col>
-            </Form.Group>
-
-            <Form.Group as={Row} controlId="formPlaintextPassword">
-              <Form.Label column sm="2">
-                Date Applied:
-              </Form.Label>
-              <Col sm="10">
-                <Form.Control type="password" placeholder="Date" />
-              </Col>
-            </Form.Group>
-          </Form>
+          <Form />
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
