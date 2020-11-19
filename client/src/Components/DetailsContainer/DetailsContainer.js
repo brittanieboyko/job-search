64387875262dc45a80c27555241b04c64c676226
brittanieb <brittanie.boyko@gmail.com>
2020-11-19 14:04:35 -0500

    display job details on details page

--- client/src/Components/DetailsContainer/DetailsContainer.js
+++ client/src/Components/DetailsContainer/DetailsContainer.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Card";
 
-const DetailsContainer = (props) => {
-  console.log(props)
-    return <h2>Details Container</h2>;
-  }
+const DetailsContainer = ({ job }) => {
+  return (
+    <Card className="text-center">
+      <Card.Header>{job.title}</Card.Header>
+      <Card.Body>
+        <Card.Text>Applied on: {job.dateApplied}</Card.Text>
+      </Card.Body>
+      <Card.Body>
+        <Button variant="light" onClick={() => console.log("hay!")}>
+          Delete job
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default DetailsContainer;
