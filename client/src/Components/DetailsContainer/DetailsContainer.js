91dc9586b933ea94a66205b5dc63ec60eecb29f6
brittanieb <brittanie.boyko@gmail.com>
2020-11-23 12:08:29 -0500

    moved details display information from detailscontainer to details header

--- client/src/Components/DetailsContainer/DetailsContainer.js
+++ client/src/Components/DetailsContainer/DetailsContainer.js
@@ -1,18 +1,9 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
-import Form from "react-bootstrap/Form";
+import DetailsHeader from "../DetailsHeader/DetailsHeader";
 
 const DetailsContainer = ({ job }) => {
   return (
-    <Card className="text-center">
-      <Card.Header>{job.title}</Card.Header>
-      <Card.Body>
-        <Card.Text>Applied on: {job.dateApplied}</Card.Text>
-      </Card.Body>
-      <Form.Group controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Application Completed" value="Application Completed" />
-      </Form.Group>
-    </Card>
+    <DetailsHeader job={job} />
   );
 };
 
