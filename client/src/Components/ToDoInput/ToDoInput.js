bfa4c3f306f2f7622e738c37bdcf411f5c6339ec
brittanieb <brittanie.boyko@gmail.com>
2020-12-09 16:09:10 -0500

    created new compent for To Do input

---
+++ client/src/Components/ToDoInput/ToDoInput.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { TextInput } from "evergreen-ui";
+
+const ToDoInput = () => {
+  return (
+    <TextInput name="todo" placeholder="Create a new list item" />
+  );
+};
+
+export default ToDoInput;
