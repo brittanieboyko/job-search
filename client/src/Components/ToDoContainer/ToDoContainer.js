2730b4f6025066783944109c83244bed9ee5cfbb
brittanieb <brittanie.boyko@gmail.com>
2020-12-09 16:53:58 -0500

    set up API methods to get all todos on page load

--- client/src/Components/ToDoContainer/ToDoContainer.js
+++ client/src/Components/ToDoContainer/ToDoContainer.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Pane } from "evergreen-ui";
 import ToDoInput from "../ToDoInput/ToDoInput";
+import api from "../../api";
 
 const ToDoContainer = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    loadToDos();
+  }, []);
+
+  const loadToDos = () => {
+    api
+      .getAllToDos()
+      .then((res) => {
+        setTodos(res.data.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const deleteToDo = (id) => {
+    api
+      .deleteToDoById(id)
+      .then(() => loadToDos())
+      .catch((err) => console.log(err));
+  };
   return (
     <Pane>
       <ToDoInput />
