63abfba2858e12abb717dbfb56c465e93b80fffa
brittanieb <brittanie.boyko@gmail.com>
2020-12-09 16:20:09 -0500

    added toDo API routes for client side usage

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,12 +3,20 @@ import axios from 'axios'
 const api = axios.create({
     baseURL: 'http://localhost:3001/api',
 })
-
+// ==== Job Routes ==== //
 export const insertJob = payload => api.post(`/job`, payload)
 export const getAllJobs = () => api.get(`/jobs`)
 export const updateJobById = (id, payload) => api.put(`/job/${id}`, payload)
 export const deleteJobById = id => api.delete(`/job/${id}`)
 export const getJobById = id => api.get(`/job/${id}`)
+
+// ==== ToDo Routes ==== //
+
+export const insertToDo = payload => api.post(`/todo`, payload)
+export const getAllToDos = () => api.get(`/todos`)
+export const updateToDoById = (id, payload) => api.put(`/todo/${id}`, payload)
+export const deleteToDoById = id => api.delete(`/todo/${id}`)
+export const getToDoById = id => api.get(`/todo/${id}`)
 
 const apis = {
     insertJob,
@@ -16,6 +24,11 @@ const apis = {
     updateJobById,
     deleteJobById,
     getJobById,
+    insertToDo,
+    getAllToDos,
+    updateToDoById,
+    deleteToDoById,
+    getToDoById
 }
 
 export default apis
