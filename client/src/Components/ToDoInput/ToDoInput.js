c109b0e1be1eba4cab912a8ac8773e355908c9a5
brittanieb <brittanie.boyko@gmail.com>
2020-12-10 16:59:00 -0500

    changed textinputfield to textInput within a FormField in ToDoInput component

--- client/src/Components/ToDoInput/ToDoInput.js
+++ client/src/Components/ToDoInput/ToDoInput.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { Pane, TextInputField, Button } from "evergreen-ui";
+import { Pane, TextInput, Button, FormField } from "evergreen-ui";
 
 const ToDoInput = ({ handleChange, handleSubmit }) => {
   return (
-      <Pane>
-        <TextInputField
-        label="Enter a new todo item"
-        description="This is a description."
-        placeholder="Placeholder text"
-        onChange={handleChange}
-        />
-        <Button type="submit" onClick={handleSubmit}>Submit</Button>
-      </Pane>
+    <Pane>
+      <FormField label="Add a to do item">
+        <TextInput placeholder="Links, notes, etc" onChange={handleChange} />
+        <Button type="submit" onClick={handleSubmit}>
+          Submit
+        </Button>
+      </FormField>
+    </Pane>
   );
 };
 
