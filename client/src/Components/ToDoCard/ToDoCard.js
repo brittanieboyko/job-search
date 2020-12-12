abea66ea74b6d700b55b6c5c1f979f71433bc944
brittanieb <brittanie.boyko@gmail.com>
2020-12-11 20:41:22 -0500

    wrapped todocard in draggable component

--- client/src/Components/ToDoCard/ToDoCard.js
+++ client/src/Components/ToDoCard/ToDoCard.js
@@ -1,23 +1,26 @@
 import React from "react";
 import { Button, Pane, Heading } from "evergreen-ui";
+import Draggable from 'react-draggable';
 
-const ToDoCard = ({ todo, onClick }) => {
+const ToDoCard = ({ todo, onClick, updatePosition }) => {
   return (
-    <Pane
-      elevation={1}
-      float="left"
-      width={200}
-      height={120}
-      margin={24}
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      flexDirection="column"
-      background="tint2"
-    >
-      <Heading>{todo.title}</Heading>
-      <Button onClick={onClick}>Delete todo</Button>
-    </Pane>
+    <Draggable>
+      <Pane
+        elevation={1}
+        float="left"
+        width={200}
+        height={120}
+        margin={24}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+        background="tint2"
+      >
+        <Heading>{todo.title}</Heading>
+        <Button onClick={onClick}>Delete todo</Button>
+      </Pane>
+     </Draggable>
   );
 };
 
