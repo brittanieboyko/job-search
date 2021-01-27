3c387cb6dd9b3c0ee9a2797617989a187cb4331f
brittanieb <brittanie.boyko@gmail.com>
2021-01-27 08:56:12 -0500

    added salary to jobs-model

--- server/models/jobs-model.js
+++ server/models/jobs-model.js
@@ -5,6 +5,7 @@ const Job = new Schema(
     {
         title: { type: String, required: true },
         dateApplied: { type: String, required: true },
+        salary: { type: Number, required: false },
         completed: { type: Boolean, required: false },
         createdAt: { type: Date, default: Date.now },
         updatedAt: { type: Date, default: Date.now }
