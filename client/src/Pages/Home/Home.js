29098bba25ce0d6bdf4bdb1d9587467cad23962d
brittanieb <brittanie.boyko@gmail.com>
2021-02-01 18:02:31 -0500

    imported navbar into home page

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import JobsCardContainer from '../../Components/JobsCardContainer/JobsCardContainer';
+import NavBar from '../../Components/NavBar/NavBar';
 import { Pane, Heading } from "evergreen-ui";
 
 const Home = () => {
     return (
       <Pane>
+        <NavBar />
         <Heading size={900}>Job Buddy</Heading>
         <>
           <JobsCardContainer />
