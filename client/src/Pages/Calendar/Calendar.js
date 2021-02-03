64a37a2728b34c3736dbbaa046ff58c6536b8466
brittanieb <brittanie.boyko@gmail.com>
2021-02-03 14:11:57 -0500

    built out basic h2 in profile and calendar pages

---
+++ client/src/Pages/Calendar/Calendar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Pane } from 'evergreen-ui';
+
+const Calendar = () => {
+  return (
+    <Pane>
+      <h2>Calendar page</h2>
+    </Pane>
+  );
+}
+
+export default Calendar;
---
+++ client/src/Pages/Profile/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Pane } from 'evergreen-ui';
+
+const Profile = () => {
+  return (
+    <Pane>
+      <h2>Profile page</h2>
+    </Pane>
+  );
+}
+
+export default Profile;
