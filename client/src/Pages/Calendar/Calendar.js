f4420450dc558eec21c8ed1d6d567b6090a77371
brittanieb <brittanie.boyko@gmail.com>
2021-02-09 18:59:54 -0500

    added discovery doc google cal scope links to calendar component

--- client/src/Pages/Calendar/Calendar.js
+++ client/src/Pages/Calendar/Calendar.js
@@ -3,6 +3,9 @@ import NavBar from "../../Components/NavBar/NavBar";
 import { Pane, Heading, Button } from 'evergreen-ui';
 
 const Calendar = () => {
+  const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+  const SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+  const gapi = window.gapi;
 
   const handleClick = () => {
     console.log("hi");
