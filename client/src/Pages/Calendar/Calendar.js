4936a0a4389422227c054ef9181441a3148ca76a
brittanieb <brittanie.boyko@gmail.com>
2021-02-09 18:55:49 -0500

    added add event button to calendar page

--- client/src/Pages/Calendar/Calendar.js
+++ client/src/Pages/Calendar/Calendar.js
@@ -1,12 +1,18 @@
 import React from "react";
 import NavBar from "../../Components/NavBar/NavBar";
-import { Pane, Heading } from 'evergreen-ui';
+import { Pane, Heading, Button } from 'evergreen-ui';
 
 const Calendar = () => {
+
+  const handleClick = () => {
+    console.log("hi");
+  }
+
   return (
     <Pane>
       <Heading size={900}>Calendar Page</Heading>
       <NavBar />
+      <Button onClick={handleClick}>Add Event</Button>
     </Pane>
   );
 }
