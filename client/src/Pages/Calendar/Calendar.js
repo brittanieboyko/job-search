c96c36022f81ad789360e1cc97ed51e662659521
brittanieb <brittanie.boyko@gmail.com>
2021-02-15 18:00:25 -0500

    defined events state variable

--- client/src/Pages/Calendar/Calendar.js
+++ client/src/Pages/Calendar/Calendar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "../../Components/NavBar/NavBar";
 import { Pane, Heading, Button } from 'evergreen-ui';
 
@@ -6,6 +6,7 @@ const Calendar = () => {
   const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
   const SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
   const gapi = window.gapi;
+  const [events, setEvents] = useState([]);
 
   const handleClick = () => {
     gapi.load("client:auth2", () => {
