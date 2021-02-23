126a6cd2c414f451af732ede0afff181a3e86bd1
brittanieb <brittanie.boyko@gmail.com>
2021-02-23 14:16:15 -0500

    added navbar to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,29 +5,33 @@ import Details from "./Pages/Details/Details";
 import NoMatch from "./Pages/NoMatch/NoMatch";
 import Profile from "./Pages/Profile/Profile";
 import Calendar from "./Pages/Calendar/Calendar";
+import NavBar from "./Components/NavBar/NavBar";
+import { Heading } from "evergreen-ui";
 
 const App = () => {
   return (
     <Router>
-        <Switch>
-          <Route path="/details">
-            <Details />
-          </Route>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/profile">
-            <Profile />
-          </Route>
-          <Route exact path="/calendar">
-            <Calendar />
-          </Route>
-          <Route path="*">
-            <NoMatch />
-          </Route>
-        </Switch>
+      <Heading size={900}>Job Buddy</Heading>
+      <NavBar />
+      <Switch>
+        <Route path="/details">
+          <Details />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/profile">
+          <Profile />
+        </Route>
+        <Route exact path="/calendar">
+          <Calendar />
+        </Route>
+        <Route path="*">
+          <NoMatch />
+        </Route>
+      </Switch>
     </Router>
   );
-}
+};
 
 export default App;
