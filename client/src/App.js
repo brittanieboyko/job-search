a5a36ce52348beef9a5df1d37a035bda80b806ef
brittanieb <brittanie.boyko@gmail.com>
2021-02-23 14:42:50 -0500

    added flex display to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,31 +6,35 @@ import NoMatch from "./Pages/NoMatch/NoMatch";
 import Goals from "./Pages/Goals/Goals";
 import Calendar from "./Pages/Calendar/Calendar";
 import NavBar from "./Components/NavBar/NavBar";
-import { Heading } from "evergreen-ui";
+import { Heading, Pane } from "evergreen-ui";
 
 const App = () => {
   return (
-    <Router>
-      <Heading size={900}>Job Buddy</Heading>
-      <NavBar />
-      <Switch>
-        <Route path="/details">
-          <Details />
-        </Route>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/goals">
-          <Goals />
-        </Route>
-        <Route exact path="/calendar">
-          <Calendar />
-        </Route>
-        <Route path="*">
-          <NoMatch />
-        </Route>
-      </Switch>
-    </Router>
+    <Pane>
+      <Router>
+        <Heading size={900}>Job Buddy</Heading>
+        <Pane display="flex">
+          <NavBar />
+          <Switch>
+            <Route path="/details">
+              <Details />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/goals">
+              <Goals />
+            </Route>
+            <Route exact path="/calendar">
+              <Calendar />
+            </Route>
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Pane>
+      </Router>
+    </Pane>
   );
 };
 
