aa4422a458a38937a79191f53a4a9aab8b2bc2b8
brittanieb <brittanie.boyko@gmail.com>
2021-03-06 21:55:50 -0500

    removed salary from db

--- server/models/jobs-model.js
+++ server/models/jobs-model.js
@@ -6,7 +6,6 @@ const Job = new Schema(
         companyName: { type: String, required: true },
         title: { type: String, required: true },
         dateApplied: { type: String, required: false },
-        salary: { type: String, required: false },
         createdAt: { type: Date, default: Date.now },
         updatedAt: { type: Date, default: Date.now }
     },
