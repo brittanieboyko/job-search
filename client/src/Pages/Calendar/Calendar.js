468fece754138a4854e91e023f2a8f1f36d2f683
brittanieb <brittanie.boyko@gmail.com>
2021-03-16 17:23:07 -0400

    separated  gapi auth logic into multiple functions and signed user in on page load

--- client/src/Pages/Calendar/Calendar.js
+++ client/src/Pages/Calendar/Calendar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CreateEventModal from "../../Components/CreateEventModal/CreateEventModal";
 import { Pane } from "evergreen-ui";
 import { Calendar, momentLocalizer } from "react-big-calendar";
@@ -15,6 +15,79 @@ const MyCalendar = () => {
   const gapi = window.gapi;
   const [valueObject, setValueObject] = useState([]);
   // const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    handleClientLoad();
+  });
+
+  const handleClientLoad = () => {
+    gapi.load("client:auth2", initClient);
+  };
+
+  const initClient = () => {
+    gapi.client
+      .init({
+        apiKey: process.env.REACT_APP_API_KEY,
+        clientId: process.env.REACT_APP_CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES,
+      })
+      .then(
+        function () {
+          // Listen for sign-in state changes.
+          gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+          // Handle the initial sign-in state.
+          updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+          // authorizeButton.onclick = handleAuthClick;
+          // signoutButton.onclick = handleSignoutClick;
+        },
+        function (error) {
+          console.log(JSON.stringify(error, null, 2));
+        }
+      );
+  };
+
+  const updateSigninStatus = (isSignedIn) => {
+    if (isSignedIn) {
+      // authorizeButton.style.display = 'none';
+      // signoutButton.style.display = 'block';
+      console.log("signed in");
+      listUpcomingEvents();
+    } else {
+      // authorizeButton.style.display = 'block';
+      // signoutButton.style.display = 'none';
+      console.log("not signed in");
+    }
+  };
+
+  // function handleSignoutClick(event) {
+  //   gapi.auth2.getAuthInstance().signOut();
+  // }
+
+  // function handleAuthClick(event) {
+  //   gapi.auth2.getAuthInstance().signIn();
+  // }
+
+  function listUpcomingEvents() {
+    gapi.client.calendar.events
+      .list({
+        calendarId: "primary",
+        timeMin: new Date().toISOString(),
+        showDeleted: false,
+        singleEvents: true,
+        maxResults: 10,
+        orderBy: "startTime",
+      })
+      .then(function (response) {
+        var events = response.result.items;
+
+        if (events.length > 0) {
+          console.log(events);
+        } else {
+          console.log("No upcoming events found.");
+        }
+      });
+  }
 
   const handleChange = (event) => {
     const { name, value } = event.target;
