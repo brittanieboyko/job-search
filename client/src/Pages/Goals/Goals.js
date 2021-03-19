ece8486802657cf06d6ce491183cdc92fd446bb7
brittanieb <brittanie.boyko@gmail.com>
2021-03-19 08:54:49 -0400

    created basic layout for three columns (year, month, day) on goals page

--- client/src/Pages/Goals/Goals.js
+++ client/src/Pages/Goals/Goals.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { Pane } from "evergreen-ui";
+import "./Goals.css";
 
 const Goals = () => {
-  return <Pane>Goals Page</Pane>;
+  return (
+    <Pane className="row">
+      <Pane className="column">Today</Pane>
+      <Pane className="column">This Month</Pane>
+      <Pane className="column">This Year</Pane>
+    </Pane>
+    );
 };
 
 export default Goals;
