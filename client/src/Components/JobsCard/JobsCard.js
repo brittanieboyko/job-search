e9c58f77c0e6941fe8fa62140adcc9bc9ff8a45b
Brittanie <brittanie.boyko@gmail.com>
2021-08-18 10:47:39 -0400

    created jobscard component with bootstrap to replace evergreen pane

--- client/src/Components/JobsCard/JobsCard.js
+++ client/src/Components/JobsCard/JobsCard.js
@@ -1,37 +1,34 @@
 import React from "react";
-import { Button, Pane, Text, Heading } from "evergreen-ui";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import "./JobsCard.css";
 
 const JobsCard = ({ job, onClick }) => {
   return (
-    <Pane
-      elevation={1}
-      float="center"
-      width={550}
-      height={120}
-      margin={24}
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      flexDirection="row"
-      background="tint2"
-    >
-      <Heading margin={12}>{job.title}, {job.companyName}</Heading>
-      <Text marginRight={12}>{job.dateApplied} </Text>
-      <Button marginRight={12}>
-        <Link
-          className="details"
-          to={{
-            pathname: "/details",
-            state: { job: job },
-          }}
-        >
-          See details
-        </Link>
-      </Button>
-      <Button onClick={onClick}>Delete job</Button>
-    </Pane>
+    <Container>
+      <Card>
+        <Card.Title>
+          {job.title}, {job.companyName}
+        </Card.Title>
+        <Card.Subtitle>{job.dateApplied} </Card.Subtitle>
+        <Button variant="light">
+          <Link
+            className="details"
+            to={{
+              pathname: "/details",
+              state: { job: job },
+            }}
+          >
+            See details
+          </Link>
+        </Button>
+        <Button variant="light" onClick={onClick}>
+          Delete job
+        </Button>
+      </Card>
+    </Container>
   );
 };
 
