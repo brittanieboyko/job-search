54a9baefa3a5a310141db8acb2c0715130f1d3fe
Brittanie <brittanie.boyko@gmail.com>
2021-08-18 10:38:01 -0400

    replaced evergreen pane with bootstrap card to create add job button on home page

--- client/src/Components/AddJobCard/AddJobCard.js
+++ client/src/Components/AddJobCard/AddJobCard.js
@@ -1,35 +1,23 @@
 import React from "react";
 import "./AddJobCard.css";
-import { Button, Pane } from "evergreen-ui";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const AddJobCard = ({ onClick }) => {
   return (
-    <Pane
-      elevation={1}
-      float="center"
-      width={550}
-      height={120}
-      margin={24}
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      flexDirection="row"
-      background="tint2"
-    >
-      <Button
-        height={24}
-        marginLeft={16}
-        appearance="minimal"
-        onClick={onClick}
-        fontSize={24}
-        fontFamily={"'Space Grotesk', sans-serif"}
-      >
-        <FontAwesomeIcon className="add-icon" icon={faPlus} />
-        Add A Job
-      </Button>
-    </Pane>
+    <Container>
+      <Card>
+        <Card.Body>
+          <Button onClick={onClick}>
+            <FontAwesomeIcon className="add-icon" icon={faPlus} />
+            Add A Job
+          </Button>
+        </Card.Body>
+      </Card>
+    </Container>
   );
 };
 
