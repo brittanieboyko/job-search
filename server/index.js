466944549714428fd72c8e734ebb33aa1374dff3
Brittanie <brittanie.boyko@gmail.com>
2021-08-26 20:09:04 -0400

    updated file path in index.js

--- server/index.js
+++ server/index.js
@@ -15,20 +15,18 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 app.use(express.static(path.join(__dirname, "client/build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  res.sendFile(path.join(__dirname + "../client/build/index.html"));
 });
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.use("/api", apiRouter);
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 module.exports = app;
