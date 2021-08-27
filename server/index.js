4a7be5e069506bbb60882ec5b0cb651e38ca168f
Brittanie <brittanie.boyko@gmail.com>
2021-08-26 20:02:36 -0400

    readded routes in index.js for serving the static build

--- server/index.js
+++ server/index.js
@@ -1,29 +1,34 @@
 require("dotenv").config();
 
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 const port = process.env.PORT || 3001;
-const logger = require('morgan');
-const db = require('./db/db');
+const logger = require("morgan");
+const db = require("./db/db");
+const path = require("path");
 
-const apiRouter = require('./routes/apiRoutes');
+const apiRouter = require("./routes/apiRoutes");
 
-const app = express()
+const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors())
+app.use(cors());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+app.use(express.static(path.join(__dirname, "client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-app.use('/api', apiRouter);
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use("/api", apiRouter);
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(port, () => console.log(`Server running on port ${port}`));
 module.exports = app;
